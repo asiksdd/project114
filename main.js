ace23a024ced974b2a1112f8dc97f452d05696cd
asiksdd <74223692+asiksdd@users.noreply.github.com>
2021-01-26 14:51:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(270,270);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+
+    save("MyFilterImage.png");
+}
